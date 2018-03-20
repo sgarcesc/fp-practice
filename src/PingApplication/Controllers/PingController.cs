91c45ef45c98eaa8355bdf4d1adfe7d310c449a1
Sebastian Garces <sgarcesc@gmail.com>
2018-03-20 02:14:05 -0500

    request response application

---
+++ src/PingApplication/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNatsClient;
+
+namespace PingApplication.Controllers
+{
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        [HttpPut]
+        public async Task<string> Put()
+        {
+            //Configure the NATS client connection
+            var cnInfo = new ConnectionInfo(new Host("nats.cloudapp.net"));
+            var client = new NatsClient("request-response", cnInfo);
+            client.Connect();
+
+            var response = await client.RequestAsync("mensaje-emitido", "PING_MESSAGE");
+            return response.GetPayloadAsString();
+        }
+    }
+}
---
+++ src/PongApplication/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MyNatsClient;
+
+namespace PongApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            //Configure the NATS client connection
+            var cnInfo = new ConnectionInfo(new Host("nats.cloudapp.net"));
+            var client = new NatsClient("request-response", cnInfo);
+            client.Connect();
+
+            await client.SubWithHandlerAsync("mensaje-emitido", msg =>
+            {
+                Thread.Sleep((int)TimeSpan.FromSeconds(2).TotalMilliseconds);
+                client.Pub(msg.ReplyTo, "PONG_MESSAGE");
+            });
+        }
+    }
+}
